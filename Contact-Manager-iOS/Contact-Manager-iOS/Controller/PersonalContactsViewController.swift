b0ba568916c90e4ed52b9728aa1b6fb7504082b5
Andy An <andyanappdev@gmail.com>
2024-01-07 00:41:29 +0900

    feat: Implement PersonalContactsViewController class

---
+++ Contact-Manager-iOS/Contact-Manager-iOS/Controller/PersonalContactsViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Contact-Manager-iOS
+//
+//  Created by Doyoung An on 1/3/24.
+//
+
+import UIKit
+
+final class PersonalContactsViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let personalContactManager: PersonalContactManager = PersonalContactManagerMock()
+    private let personalContactsView: PersonalContactsView = PersonalContactsView()
+    
+    
+    // MARK: - Life Cycle
+    override func loadView() {
+    view = personalContactsView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureTableView()
+    }
+    
+    
+    // MARK: - Helpers Funtions
+    private func configureTableView() {
+        personalContactsView.personalContactsTableView.dataSource = self
+        personalContactsView.personalContactsTableView.register(PersonalContactTableViewCell.self, forCellReuseIdentifier: "PersonalContactTableViewCell")
+    }
+}
+
+
+// MARK: - Extention
+extension PersonalContactsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        personalContactManager.showUpAllPersonalContacts().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let reusableCell = tableView.dequeueReusableCell(withIdentifier: "PersonalContactTableViewCell", for: indexPath) as? PersonalContactTableViewCell else {
+            fatalError("Error: Cannot dequeue resuable cell")
+        }
+        
+        reusableCell.personalContact = personalContactManager.showUpAllPersonalContacts()[indexPath.row]
+        
+        return reusableCell
+    }
+}
