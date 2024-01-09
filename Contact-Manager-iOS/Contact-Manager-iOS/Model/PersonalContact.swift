fed2b14bcd61a518aad6f33fbdf74e399f0c7add
Andy An <andyanappdev@gmail.com>
2024-01-09 11:27:20 +0900

    refactor: Change PersonalContact data model property data type
    - Remove optional to treat name property as required input

--- Contact-Manager-iOS/Contact-Manager-iOS/Model/PersonalContact.swift
+++ Contact-Manager-iOS/Contact-Manager-iOS/Model/PersonalContact.swift
@@ -8,11 +8,11 @@
 struct PersonalContact {
     static var personalContactsCounting: Int = 0
     let id: Int
-    var name: String?
+    var name: String
     var age: String?
     var contactNumber: String?
     
-    init(name: String? = nil, age: String? = nil, contactNumber: String? = nil) {
+    init(name: String, age: String? = nil, contactNumber: String? = nil) {
         self.id = PersonalContact.personalContactsCounting == 0 ? 0 : PersonalContact.personalContactsCounting
         self.name = name
         self.age = age
