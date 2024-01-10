c68a2ebdb570f9642590454855d559f579c2e4c5
Andy An <andyanappdev@gmail.com>
2024-01-10 11:35:08 +0900

    refactor: Change variable name

--- Contact-Manager-iOS/Contact-Manager-iOS/App/SceneDelegate.swift
+++ Contact-Manager-iOS/Contact-Manager-iOS/App/SceneDelegate.swift
@@ -17,9 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let navigationViewController = UINavigationController(rootViewController: PersonalContactsViewController())
+        let PersonalContactsNavigationViewController = UINavigationController(rootViewController: PersonalContactsViewController())
         
-        window?.rootViewController = navigationViewController
+        window?.rootViewController = PersonalContactsNavigationViewController
         window?.makeKeyAndVisible()
     }
 
