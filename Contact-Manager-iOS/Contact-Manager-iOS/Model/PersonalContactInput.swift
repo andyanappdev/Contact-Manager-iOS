9b3c3e6ab8b138dd4590654f383402dbe798fb5d
Andy An <andyanappdev@gmail.com>
2024-01-18 09:33:19 +0900

    feat: Define PersonalContactInput data model

---
+++ Contact-Manager-iOS/Contact-Manager-iOS/Model/PersonalContactInput.swift
@@ -0,0 +1,12 @@
+//
+//  PersonalContactInput.swift
+//  Contact-Manager-iOS
+//
+//  Created by Doyoung An on 1/18/24.
+//
+
+struct PersonalContactInput {
+    let name: String
+    let age: String
+    let contactNumber: String
+}
